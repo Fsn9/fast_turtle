a7f47044d6393eda6cac939df6d516de0c6f41cd
francisco <fsn9.astro@gmail.com>
2021-07-30 17:05:07 +0100

    example.cpp changed

--- scripts/example.cpp
+++ scripts/example.cpp
@@ -1,27 +1,27 @@
-// Simulator classes
-#include "fast_turtle.h"
-#include <ros/ros.h>
+#include "fast_turtle.h" // The simulator library
+#include <ros/ros.h> // The ROS library if you want to use it
 
-int main(int argc, char **argv)
+int main()
 {
-    FastTurtle ft;
-    float obstacle_radius = 0.15;
-    ft.init_world(4, 0, 0,"square");
-    ft.add_turtlebot_burger(0, -1, -M_PI_2, 0.09, 1, "michelangelo");
-    ft.add_obstacle(0, -2, obstacle_radius, "round", false);
-    ft.add_obstacle(0, 2, obstacle_radius, "round", false);
-    ft.add_obstacle(-1, -1, obstacle_radius, "round", false);
-    ft.add_obstacle(-1, -2, obstacle_radius, "round", false);
-    float v = 0.1;
-    float w = 0.0;
-    int idx_robot = 0;
-    Observation observation;
+ FastTurtle ft; // The simulator object
+ ft.init_world(4, 0, 0,"square"); // Initializing the world
+ // Adding a robot to the world (x=0,y=-1,orientation=-M_PI/2,radius=0.09m,cycle_time=1s,name="michelangelo")
+ ft.add_turtlebot_burger(0, -1, -M_PI_2, 0.09, 1, "michelangelo"); 
+ // Adding an obstacle (x=0, y=-2, radius=0.1m, type="round", dynamics = false)
+ ft.add_obstacle(0, -2, 0.1, "round", false); 
+ // Defining some twist commands to act in the world
+ float v = 0.1; //linear velocity
+ float w = 0.0; //angular velocity
+ // Define the observation object (where we collect the observations of the robot)
+ Observation observation;
+ 
+ // The main loop of the simulator
+ while(1){
+  ft.act(v,w,0); // Robot 0 Acting with twist commands in the world
+  observation = ft.observe(0); // Observation of the robot 0 created in line 37 (the only robot stored in the vector of robots in the first position) 
+  observation.print(); // Print the observation to the terminal for debugging
+ }
 
-    while(1){
-        ft.act(v, w, idx_robot);
-        observation = ft.observe(idx_robot);
-        observation.print();
-    }
-    
-    return 0;
+
+  return 0;
 }
