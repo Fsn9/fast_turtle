9617859ec9725b6e7bf56fa3a82b80be708f0b26
francisco <fsn9.astro@gmail.com>
2021-10-09 00:49:01 +0100

    Upgrade in Controller class. Two new derived classes from Controller that have specific functionalities depending on the way they control: vx,vy for simple_drone and v,w for turtlebot_burgers

--- include/controller.h
+++ include/controller.h
@@ -6,32 +6,50 @@
 #include "fast_turtle/RobotDataArray.h"
 
 typedef struct robot_data{
-    double pos_x; 
-    double pos_y;
-    double quat_x;
-    double quat_y;
-    double quat_z;
-    double quat_w;
+    double pos_x_; 
+    double pos_y_;
+    double quat_x_;
+    double quat_y_;
+    double quat_z_;
+    double quat_w_;
+    std::vector<float> laser_scan_;
 } robot_data;
 
 class Controller{
-    protected:
-        ros::NodeHandle nh_;
-        ros::Subscriber listen_robot_subscriber_;
-        ros::Publisher cmd_vel_publisher_;
-        robot_data robot_data_{0,0,0,0,0,0};
-        std::vector<float> robot_laser_scan_;
-        int robot_idx_;
     public:
-        Controller(int robot_idx);
+        Controller(int robot_idx, std::string topic_to_publish);
+        virtual void decide() = 0;
+        virtual void publish() = 0;
+    protected:
+        robot_data get_robot_data();
+        ros::Publisher cmd_vel_publisher_;
+        virtual void update_commands(double, double) = 0;
+    private:
+        robot_data robot_data_{0,0,0,0,0,0};
+        void listen(const fast_turtle::RobotDataArray& robots_msg);
+        int robot_idx_;
+        ros::NodeHandle nh_;
+        ros::Subscriber listener_;
+};
 
-        // Listens to /robots topic
-        void listen_robot(const fast_turtle::RobotDataArray& robots_msg);
+class SimpleDroneController : public Controller{
+    public:
+        SimpleDroneController(int idx, std::string topic) : Controller(idx, topic){}
+        void publish() override;
+    protected:
+        void update_commands(double vx, double vy) override;
+    private:
+        float cmd_vx_{0}, cmd_vy_{0};
+};
 
-        // Function of deciding commands to be implemented
-        virtual cmd_vel_tbb decide_commands() = 0;
-
-        // Publishes commands into /cmd_vel##idx topic
-        void publish_commands(double v, double w);
+class TurtlebotBurgerController : public Controller{
+    
+    public:
+        TurtlebotBurgerController(int idx, std::string topic) : Controller(idx, topic){}
+        void publish() override;
+    protected:
+        void update_commands(double v, double w) override;
+    private:
+        float cmd_v_{0}, cmd_w_{0};
 };
 #endif // CONTROLLER_H
--- src/controller.cpp
+++ src/controller.cpp
@@ -2,33 +2,53 @@
 #include "geometry_msgs/Twist.h"
 #include "fast_turtle/RobotData.h"
 
-Controller::Controller(int robot_idx)
+Controller::Controller(int robot_idx, std::string topic_to_publish)
 {
     robot_idx_ = robot_idx;
-    cmd_vel_publisher_ = nh_.advertise<geometry_msgs::Twist>("cmd_vel" + std::to_string(robot_idx_), 1000);
-    listen_robot_subscriber_ = nh_.subscribe("robots", 10, &Controller::listen_robot, this);
+    cmd_vel_publisher_ = nh_.advertise<geometry_msgs::Twist>(topic_to_publish, 100);
+    listener_ = nh_.subscribe("turtlebot_burgers", 10, &Controller::listen, this);
 }
 
-void Controller::publish_commands(double v, double w)
+robot_data Controller::get_robot_data()
+{
+    return robot_data_;
+}
+
+void Controller::listen(const fast_turtle::RobotDataArray& robots_msg)
+{
+    robot_data_.pos_x_ = robots_msg.robots[robot_idx_].pose.position.x;
+    robot_data_.pos_y_ = robots_msg.robots[robot_idx_].pose.position.y;
+    robot_data_.quat_x_ = robots_msg.robots[robot_idx_].pose.orientation.x;
+    robot_data_.quat_y_ = robots_msg.robots[robot_idx_].pose.orientation.y;
+    robot_data_.quat_z_ = robots_msg.robots[robot_idx_].pose.orientation.z;
+    robot_data_.quat_w_ = robots_msg.robots[robot_idx_].pose.orientation.w;
+    robot_data_.laser_scan_ = robots_msg.robots[robot_idx_].scan.ranges;
+}
+
+void SimpleDroneController::publish()
 {
     geometry_msgs::Twist twist_msg;
-    twist_msg.linear.x = v;
-    twist_msg.angular.z = w;
+    twist_msg.linear.x = cmd_vx_;
+    twist_msg.linear.y = cmd_vy_;
     cmd_vel_publisher_.publish(twist_msg);
 }
 
-void Controller::listen_robot(const fast_turtle::RobotDataArray& robots_msg)
+void SimpleDroneController::update_commands(double vx, double vy)
 {
-    // Position (x,y)
-    robot_data_.pos_x = robots_msg.robots[robot_idx_].pose.position.x;
-    robot_data_.pos_y = robots_msg.robots[robot_idx_].pose.position.y;
+    cmd_vx_ = vx;
+    cmd_vy_ = vy;
+}
 
-    // Quaternion of orientation (x,y,z,w)
-    robot_data_.quat_x = robots_msg.robots[robot_idx_].pose.orientation.x;
-    robot_data_.quat_y = robots_msg.robots[robot_idx_].pose.orientation.y;
-    robot_data_.quat_z = robots_msg.robots[robot_idx_].pose.orientation.z;
-    robot_data_.quat_w = robots_msg.robots[robot_idx_].pose.orientation.w;
+void TurtlebotBurgerController::publish()
+{
+    geometry_msgs::Twist twist_msg;
+    twist_msg.linear.x = cmd_v_;
+    twist_msg.angular.z = cmd_w_;
+    cmd_vel_publisher_.publish(twist_msg);
+}
 
-    // Laser scan distances (all the 360 ray distances)
-    robot_laser_scan_ = robots_msg.robots[robot_idx_].scan.ranges;
+void TurtlebotBurgerController::update_commands(double v, double w)
+{
+    cmd_v_ = v;
+    cmd_w_ = w;
 }
