c8635632c09ef25fe12b7c839e8802aedeeaf043
francisco <fsn9.astro@gmail.com>
2021-10-09 00:50:33 +0100

    my_controller example script updated taking into account the updates from the previous commit 9617859ec9725b6e7bf56fa3a82b80be708f0b26. it is controlling a drone and a robot as an example.

--- src/my_controller.cpp
+++ src/my_controller.cpp
@@ -1,26 +1,51 @@
 #include "controller.h"
 
-class MyController : public Controller
+class TBB1_Controller : public TurtlebotBurgerController
 {
     public:
-        MyController(int robot_idx) : Controller(robot_idx) {}
-        cmd_vel_tbb decide_commands()
+        TBB1_Controller(int robot_idx, std::string topic_name) : TurtlebotBurgerController(robot_idx, topic_name) {}
+        void decide()
         {
-            // Get position and orientation
-            double pos_x = robot_data_.pos_x;
-            double pos_y = robot_data_.pos_x;
-            double quat_x = robot_data_.quat_x;
-            double quat_y = robot_data_.quat_y;
-            double quat_z = robot_data_.quat_z;
-            double quat_w = robot_data_.quat_w;
-            std::vector<float> laser_scan = robot_laser_scan_;
+            // Get real time data
+            robot_data data = get_robot_data();
+            double pos_x = data.pos_x_;
+            double pos_y = data.pos_x_;
+            double quat_x = data.quat_x_;
+            double quat_y = data.quat_y_;
+            double quat_z = data.quat_z_;
+            double quat_w = data.quat_w_;
+            std::vector<float> laser_scan = data.laser_scan_;
 
-            // Decide commands
+            // Save commands to be published on publish() call
             double v = 0.22;
             double w = 0.2;
-            return cmd_vel_tbb{v,w};
+            update_commands(v, w);
         }
 };
+
+class SD0_Controller : public SimpleDroneController
+{
+    public:
+        SD0_Controller(int robot_idx, std::string topic_name) : SimpleDroneController(robot_idx, topic_name) {}
+        void decide()
+        {
+            // Get real time data
+            robot_data data = get_robot_data();
+            double pos_x = data.pos_x_;
+            double pos_y = data.pos_x_;
+            double quat_x = data.quat_x_;
+            double quat_y = data.quat_y_;
+            double quat_z = data.quat_z_;
+            double quat_w = data.quat_w_;
+            std::vector<float> laser_scan = data.laser_scan_;
+
+            // Save commands to be published on publish() call
+            double vx = 0.22;
+            double vy = 0.2;
+            update_commands(vx, vy);
+        }
+};
+
 
 int main(int argc, char **argv)
 {
@@ -28,20 +53,22 @@ int main(int argc, char **argv)
     ros::init(argc, argv, "my_controller");
 
     // Define my controller
-    int robot_idx = 4;
-    MyController my_controller(robot_idx);
+    int tbb_idx = 1;
+    int sd_idx = 0;
+    TBB1_Controller tbb1_controller(tbb_idx, "cmd_vel_tbb1");
+    SD0_Controller sd0_controller(sd_idx, "cmd_vel_sd0");
 
-    // Commands
-    cmd_vel_tbb commands;
-
-    // Publish at a loop_rate
-    int loop_rate = 3; // hz
+    // Publish at a loop_rate (in hz)
+    int loop_rate = 3; 
     ros::Rate rate(loop_rate);    
+
     // Infinite cycle to read and publish commands
     while(ros::ok())
     {
-        commands = my_controller.decide_commands();
-        my_controller.publish_commands(commands.v, commands.w);
+        tbb1_controller.decide();
+        sd0_controller.decide();
+        tbb1_controller.publish();
+        sd0_controller.publish();
         rate.sleep();
         ros::spinOnce();
     }
